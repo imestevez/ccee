aced7eeb7c8c7728bcefc351177ec100253acada
ivanme93 <i.martinezestevez@gmail.com>
2018-01-29 12:36:02 +0100

    Añadido collate a formularios

--- Functions/Access_DB.php
+++ Functions/Access_DB.php
@@ -9,6 +9,7 @@ Script que realiza la conexion a la BD
 //funcion para conectar a la BD
 function ConnectDB(){
    $conexion = mysqli_connect("localhost", "ccee", "ccee", "CCEE"); //realiza la conexion
+   mysqli_query($conexion, "SET NAMES 'utf8'");
 
  if ($conexion->connect_errno) {//si se produce un error
     	return false;
